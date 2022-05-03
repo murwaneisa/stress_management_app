838f8b8400e3da173d1c6cf088d8cfd8f708e91c
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-05-03 16:27:24 +0200

    adding DB connecting file and ignore fiile

--- stress_tracker/lib/database.py
+++ stress_tracker/lib/database.py
@@ -1,4 +1,27 @@
-# function to fetch database.
-# function to store  database 
-# function to update database 
-# function to fetch database 
+import configparser
+
+import mysql.connector
+
+config = configparser.ConfigParser()
+# read the configuration file to get the
+config.read("config.ini")
+
+
+class Database:
+    def connect_database():
+        try:
+            mydb = mysql.connector.connect(
+                host=config.get(
+                    "database", "db_host_key"
+                ),  # get the credential from the config file
+                user=config.get("database", "db_user_key"),
+                password=config.get("database", "db_password_key"),
+                database=config.get("database", "db_database_key"),
+            )
+            print(mydb)
+            print("working")
+        except Exception as er:
+            print(er)
+
+
+Database.connect_database()
