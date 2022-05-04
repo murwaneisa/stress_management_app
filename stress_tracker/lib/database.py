cabc835d47f613b483c99a9c7912673142a4a54e
smallpotato <smallpotato6035@gmail.com>
2022-05-04 15:05:29 +0200

    fix: hide database connection for added security

--- stress_tracker/lib/database.py
+++ stress_tracker/lib/database.py
@@ -1,16 +1,17 @@
 import configparser
-
 import mysql.connector
 
 config = configparser.ConfigParser()
-# read the configuration file to get the
 config.read("config.ini")
 
 
 class Database:
-    def connect_database():
+    def __init__(self):
+        self.connector = None
+
+    def connect_database(self):
         try:
-            mydb = mysql.connector.connect(
+            self.connector = mysql.connector.connect(
                 host=config.get(
                     "database", "db_host_key"
                 ),  # get the credential from the config file
@@ -18,47 +19,47 @@ class Database:
                 password=config.get("database", "db_password_key"),
                 database=config.get("database", "db_database_key"),
             )
-            return mydb
+
         except Exception as er:
             print(er)
 
-    def select_user(connect):
-        mycursor = connect.cursor()
+    def select_user(self):
+        mycursor = self.connector.cursor()
         mycursor.execute("SELECT * FROM user")
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
 
-    def select_admin(connect):
-        mycursor = connect.cursor()
+    def select_admin(self):
+        mycursor = self.connector.cursor()
         mycursor.execute("SELECT * FROM admin")
         myresult = mycursor.fetchall()
         for x in myresult:
             print(x)
 
-    def insert_user(
-        connect,
-        first_name,
-        last_name,
-        gender,
-        email,
-        program,
-        degree,
-        password,
-        age,
-        study_year,
-    ):
+    def insert_user(self,
+                    first_name,
+                    last_name,
+                    gender,
+                    email,
+                    program,
+                    degree,
+                    password,
+                    age,
+                    study_year,
+                    ):
         try:
-            mycursor = connect.cursor()
-            query = f"""INSERT INTO user (user_firstname, user_lastname, user_gender, user_email, user_program, user_degree, user_password, user_age, user_studyYear)
+            mycursor = self.connector.cursor()
+            query = f"""INSERT INTO user (user_firstname, user_lastname, user_gender,
+            user_email, user_program, user_degree, user_password, user_age, user_studyYear)
             VALUES ("{first_name}", "{last_name}", "{gender}", "{email}", "{program}", "{degree}", "{password}", "{age}", "{study_year}")"""
             mycursor.execute(query)
         except Exception as er:
             print(er)
 
-    def insert_admin(connect, first_name, last_name, email, password, title):
+    def insert_admin(self, first_name, last_name, email, password, title):
         try:
-            mycursor = connect.cursor()
+            mycursor = self.connector.cursor()
             query = f"""INSERT INTO admin (admin_firstname, admin_lastname, admin_email, admin_password, admin_title)
                 VALUES ("{first_name}", "{last_name}", "{email}", "{password}", "{title}")"""
             mycursor.execute(query)
@@ -66,19 +67,19 @@ class Database:
             print(er)
 
 
-connect = Database.connect_database()
-Database.insert_user(
-    connect,
-    "Ahmed",
-    "moh",
-    "male",
-    "ahmed@gmail.com",
-    "management",
-    "master",
-    "ahmed123",
-    "21",
-    "second year",
-)
+# connect = Database.connect_database()
+# Database.insert_user(
+#     connect,
+#     "Ahmed",
+#     "moh",
+#     "male",
+#     "ahmed@gmail.com",
+#     "management",
+#     "master",
+#     "ahmed123",
+#     "21",
+#     "second year",
+# )
 # Database.insert_admin(connect, "lisa", "adem", "lisa@gmail.com", "lisa123", "IT")
-Database.select_user(connect)
+# Database.select_user(connect)
 # Database.select_admin(connect)
