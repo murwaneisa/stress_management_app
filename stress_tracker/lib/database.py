c41426b3e0c9a7c74afb16b6d73be1309ae84c21
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-05-04 11:06:03 +0200

    adding insert functions

--- stress_tracker/lib/database.py
+++ stress_tracker/lib/database.py
@@ -18,10 +18,67 @@ class Database:
                 password=config.get("database", "db_password_key"),
                 database=config.get("database", "db_database_key"),
             )
-            print(mydb)
-            print("working")
+            return mydb
+        except Exception as er:
+            print(er)
+
+    def select_user(connect):
+        mycursor = connect.cursor()
+        mycursor.execute("SELECT * FROM user")
+        myresult = mycursor.fetchall()
+        for x in myresult:
+            print(x)
+
+    def select_admin(connect):
+        mycursor = connect.cursor()
+        mycursor.execute("SELECT * FROM admin")
+        myresult = mycursor.fetchall()
+        for x in myresult:
+            print(x)
+
+    def insert_user(
+        connect,
+        first_name,
+        last_name,
+        gender,
+        email,
+        program,
+        degree,
+        password,
+        age,
+        study_year,
+    ):
+        try:
+            mycursor = connect.cursor()
+            query = f"""INSERT INTO user (user_firstname, user_lastname, user_gender, user_email, user_program, user_degree, user_password, user_age, user_studyYear)
+            VALUES ("{first_name}", "{last_name}", "{gender}", "{email}", "{program}", "{degree}", "{password}", "{age}", "{study_year}")"""
+            mycursor.execute(query)
+        except Exception as er:
+            print(er)
+
+    def insert_admin(connect, first_name, last_name, email, password, title):
+        try:
+            mycursor = connect.cursor()
+            query = f"""INSERT INTO admin (admin_firstname, admin_lastname, admin_email, admin_password, admin_title)
+                VALUES ("{first_name}", "{last_name}", "{email}", "{password}", "{title}")"""
+            mycursor.execute(query)
         except Exception as er:
             print(er)
 
 
-Database.connect_database()
+connect = Database.connect_database()
+Database.insert_user(
+    connect,
+    "Ahmed",
+    "moh",
+    "male",
+    "ahmed@gmail.com",
+    "management",
+    "master",
+    "ahmed123",
+    "21",
+    "second year",
+)
+# Database.insert_admin(connect, "lisa", "adem", "lisa@gmail.com", "lisa123", "IT")
+Database.select_user(connect)
+# Database.select_admin(connect)
