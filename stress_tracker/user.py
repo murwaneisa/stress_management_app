4e0c2d43bd2c5ab0115516f24388ee4aceecae34
ahmedmohd957 <ahmed.mohd957@gmail.com>
2022-05-05 21:12:18 +0200

    user class added

---
+++ stress_tracker/user.py
@@ -0,0 +1,23 @@
+class User:
+    id = None
+    first_name = ""
+    last_name = ""
+    gender = ""
+    email = ""
+    program = ""
+    degree = ""
+    password = ""
+    age = None
+    study_year = None
+    
+    def __init__(self, id, first_name, last_name, gender, email, program, degree, password, age, study_year):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.email = email
+        self.program = program
+        self.degree = degree
+        self.password = password
+        self.age = age
+        self.study_year = study_year
