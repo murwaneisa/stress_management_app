379bc4c24c805b1a43202bf50578b275d28fcffe
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-05-05 23:32:00 +0200

    add login test

--- stress_tracker/lib/test_database.py
+++ stress_tracker/lib/test_database.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import Mock, patch, MagicMock
 import configparser
 import mysql.connector
+from database import Database
 
 config = configparser.ConfigParser()
 # read the configuration file to get the
@@ -22,6 +23,12 @@ class TestGame(unittest.TestCase):
             self.assertIsInstance(mydb, object)
         except Exception as er:
             print(er)
+
+    def test_userLogin(self):
+        name = "murwan@gmail.com"
+        password = "murwan123"
+        result = Database.login_user(self, name, password)
+        self.assertIsInstance(result, tuple)
 
 
 if __name__ == "__main__":
